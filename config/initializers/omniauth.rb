9e47ff9efa0360d0cf2a34abcff74427f5b09528
Diego Castañeda <diego.castaneda@sequra.es>
2018-09-11 18:06:24 +0200

    [FIX]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require "rspotify/oauth"
 
-Rails.application.config.middleware.use OmniAuth:Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret, scope: "playlist-modify-public"
 end
